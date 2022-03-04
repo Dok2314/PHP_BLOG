cbbfb0dc6fa0015feb398351344315f44f479c27
Daniil <koversundanil@gmail.com>
2022-03-04 14:11:15 +0200

    Insert And Update

--- application/database/connect.php
+++ application/database/connect.php
@@ -6,14 +6,13 @@ $db_name = 'my_blog';
 $db_user = 'root1';
 $db_pass = 'root';
 $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ];
 
 try{
     $pdo = new PDO("$driver:host=$host;dbname=$db_name",
-    $db_user,$db_pass,$options
+        $db_user,$db_pass,$options
     );
 }catch(PDOException $e){
     die('Ошибка подключения к Базе Данных!');
 }
-
--- application/database/database.php
+++ application/database/database.php
@@ -71,7 +71,71 @@ function selectOne($table, $params = [])
     return $query->fetch();
 }
 
+// Добавляю запись в БД
+
+function insert($table, $params)
+{
+    global $pdo;
+
+    $i = 0;
+    $coll = '';
+    $mask = '';
+    foreach ($params as $key => $value) {
+        if($i === 0){
+            $coll = $coll . "$key";
+            $mask = $mask . "'" . "$value" . "'";
+        }else{
+            $coll = $coll . ", $key";
+            $mask = $mask . ", '" . "$value" . "'";
+        }
+        $i++;
+    }
+    $sql = "INSERT INTO $table($coll)
+            VALUES($mask)";
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+    dbCheckError($query);
+}
+
+// Обновляю И Удаляю запись с БД
+
+function update($table, $id, $params)
+{
+    global $pdo;
+    $i = 0;
+    $str = '';
+    foreach ($params as $key => $value) {
+        if($i === 0){
+            $str = $str . $key . " = '" . $value . "'";
+        }else{
+            $str = $str . ", " . $key  . " = '" . $value . "'";
+        }
+        $i++;
+    }
+
+    $sql = "UPDATE $table
+            SET $str
+            WHERE id = $id";
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+}
+
+function delete($table, $id)
+{
+    global $pdo;
+    $sql = "DELETE FROM $table
+            WHERE id = $id";
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+}
+
 $params = [
-    'is_admin' => 1,
-    'username' => 'Dok'
+  'is_admin' => 0,
+  'password' => '777777'
 ];
+
+delete('users',16);
