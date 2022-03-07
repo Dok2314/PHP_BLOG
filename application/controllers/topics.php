e9de0d4ddfc9efe7f5d1cead05816ded5e4b9a35
Daniil <koversundanil@gmail.com>
2022-03-07 13:34:20 +0200

    Topic CRUD

--- application/controllers/topics.php
+++ application/controllers/topics.php
@@ -9,7 +9,7 @@ $name         = '';
 $description  = '';
 $allTopics    = selectAll('topics');
 
-//Код для формы регистрации
+//Создание категории
 if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['topic-create'])){
 
     $name        =    trim($_POST['name']);
